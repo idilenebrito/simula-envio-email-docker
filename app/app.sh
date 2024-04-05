d66ceaa8b09bacd82f361f115e4458db10d05eb3
Idilene Brito - IBI <idilene.brito@iteris.com.br>
2024-04-05 18:00:28 -0300

    feat: add worker

--- app/app.sh
+++ app/app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pip install bottle==0.12.25 psycopg2==2.9.1
+pip install bottle==0.12.25 psycopg2==2.9.1 redis==2.10.5
 python -u sender.py
---
+++ worker/worker.py
@@ -0,0 +1,17 @@
+import redis
+import json
+
+from time import sleep
+from random import randint
+
+if __name__ == '__main__':
+  r = redis.Redis(host='queue', port=6379, db=0)
+  
+  while True:
+    mensagem = json.loads(r.blpop('sender')[1])
+      
+    #simulando envio de email
+    print('Mandando e-mail: ', mensagem['assunto'])
+      
+    sleep(randint(15,45))
+    print('Mensagem enviada: ', mensagem['assunto'], 'enviada')
