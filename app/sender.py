8bf56bc04a0312bfff530b0f13a7ab2e9ba51b59
Idilene Brito - IBI <idilene.brito@iteris.com.br>
2024-04-08 14:02:59 -0300

    feat: add variaveis de ambientes

--- app/sender.py
+++ app/sender.py
@@ -1,15 +1,26 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 class Sender(Bottle):
   def __init__(self):
     super().__init__()
     self.route('/', method='POST', callback=self.send)
-    self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-    DSN = 'dbname=email_sender user=postgres password=postgres host=db'
-    self.conn = psycopg2.connect(DSN)
+    
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+    
+    self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+    
+    db_host = os.getenv('DB_HOST', 'db')
+    db_user = os.getenv('DB_USER', 'postgres')
+    db_name = os.getenv('DB_NAME', 'sender')
+    db_pass = os.getenv('DB_PASS', 'postgres')
+    
+    dsn = f'dbname={db_name} user={db_user} password={db_pass} host={db_host}'
+    
+    self.conn = psycopg2.connect(dsn)
     
   def register_message(self, assunto, mensagem):
     SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
@@ -19,12 +30,14 @@ class Sender(Bottle):
     cur.close()
     msg = {'assunto': assunto, 'mensagem': mensagem}
     self.fila.rpush('sender', json.dumps(msg))
+    
     print('Mensagem registrada !')
     
   def send(self):
     assunto = request.forms.get('assunto')
     mensagem = request.forms.get('mensagem')
     self.register_message(assunto, mensagem)
+    
     return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
     assunto, mensagem)
     
